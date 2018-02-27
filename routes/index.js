7e864191ac59c4770463fcbc8e342c2853272873
panforrest <guoqianp@gmail.com>
2018-02-26 21:47:52 -0500

    p3 Hitting APIs and Displaying Data, stupid bug found const https = require('http')

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,12 @@
 const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
+const https = require('https')
+const url = require('url')
 
 /*  This is the home route. It renders the index.mustache page from the views directory.
 	Data is rendered using the Mustache templating engine. For more
 	information, view here: https://mustache.github.io/#demo */
-// router.get('/', function(req, res){
-//     if(req.vertexSession.user) {
-//         res.redirect('/stocks')
-//     } else {
-//         res.render('index', {text: 'This is the dynamic data. Open index.js from the routes directory to see.'})
-//     }
-// })
 
 router.get('/', function(req, res){
     if(req.vertexSession.user) {
@@ -24,18 +19,150 @@ router.get('/', function(req, res){
     }
 })
 
+// router.get('/stocks', function(req, res) {
+//     if(req.vertexSession.user) {
+//         turbo.fetchUser(req.vertexSession.user.id)
+//         .then(data => {
+//             getStockInfo(data.stockinput).then(stockdata => {
+//                 let stockprices = []
+//                 for (let i=0; i < data.stockinput.length; i++){
+//                     stockprices.push({name: data.stockinput[i], price: stockdata[i]})
+//                 }
+//                 res.render('stocks', {stockinput: stockprices})
+//             })
+//         })
+//     } else {
+//         res.redirect('/')
+//     }
+// })
+
 router.get('/stocks', function(req, res) {
     if(req.vertexSession.user) {
         turbo.fetchUser(req.vertexSession.user.id)
-        // console.log(JSON.stringify(data.stockinput))
-        // console.log(req.vertexSession.user.id)
         .then(data => {
-            res.render('stocks', {stockinput: data.stockinput})
+            getStockInfo(data.stockinput).then(stockdata => {
+                let stockprices = []
+                for(let i = 0; i < data.stockinput.length; i++) {
+                    stockprices.push({name: data.stockinput[i], price: stockdata[i]})
+                }
+                res.render('stocks', {stockinput: stockprices})
+            })
         })
     } else {
         res.redirect('/')
     }
 })
+
+// getStockInfo = (stockinput) => {
+//     let promises = []
+
+//     stockinput.map((item) => {
+//         let URL = url.parse('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + item + '&apikey=FVAFE9CIPFN3Q0Z4')
+//         let promise = getStockPrice(URL)
+//         promises.push(promise)
+//     })
+
+//     return Promise.all(promises).then((values) => {
+//         return values
+//     })
+// }
+
+
+
+getStockInfo = (stockinput) => {
+    let promises = []
+   
+    stockinput.map((item) => {
+        let URL = url.parse('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + item + '&apikey=FVAFE9CIPFN3Q0Z4')
+        // console.log(URL)
+        let promise = getStockPrice(URL)
+        // console.log(JSON.stringify(promise))
+        promises.push(promise)
+        // console.log(JSON.stringify(promises))
+    })
+        
+    return Promise.all(promises).then((values) => {
+        return values
+    })
+}
+
+// function getStockPrice(url){
+//     let promise = new Promise(
+//     function(resolve, reject){
+//         const option = {
+//             hostname: url.host,
+//             path: url.path,
+//         }
+
+//         //Call the https get request
+//         let getRequest = https.get(option, (res) => {
+//             let fulldata
+//             res.on('data', (d) => {
+//                 if(fulldata){
+//                     fulldata += d
+//                 } else {
+//                     fulldata = d
+//                 }
+//             })
+//             res.on('end', () => {
+//                 let price
+
+//                 //parse out the JSON data and grab the price from it
+//                 try {
+//                     let stockJSONData = JSON.parse(fulldata)
+//                     if(stockJSONData) {
+//                         price = stockJSONData["Time Series (Daily)"][Object.keys(stockJSONData["Time Series (Daily)"])[0]]['4. close']
+//                     }
+//                 } catch(error){
+//                     price = "parsing error"
+//                 }
+//                 //resolve the promise to the price information
+//                 resolve(price)
+//             })
+//         })
+//     })
+//     return promise
+// }
+
+function getStockPrice(url) {
+    let promise = new Promise(
+    function(resolve, reject) {
+            
+        const option = {
+            hostname: url.host,
+            path: url.path,
+        }
+
+        //Call the https get request
+        let getRequest = https.get(option, (res) => {
+            let fulldata
+            res.on('data', (d) => {
+                if(fulldata) {
+                    fulldata += d
+                } else {
+                    fulldata = d
+                }
+            })
+            console.log(fulldata)
+            res.on('end', () => {
+                let price
+                // console.log(fulldata)
+                //Parse out the JSON data and grab the price from it
+                try {
+                    let stockJSONData = JSON.parse(fulldata)
+                    if(stockJSONData) {
+                        price = stockJSONData["Time Series (Daily)"][Object.keys(stockJSONData["Time Series (Daily)"])[0]]['4. close']
+                    }   
+                } catch (error) {
+                    price = error.message//"parsing error"
+                }
+                //resolve the promsie to the price information
+                resolve(price)
+            })
+        })
+    })
+    return promise
+}
 
 router.post('/login', function(req, res) {
     turbo.login(req.body)
